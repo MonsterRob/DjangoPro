5ce481ff13c002cdb7e9852d8c3a941ac1a370c1
MonsterRob <wmwwzhao@163.com>
2017-09-16 21:00:27 +0800

    del two line

--- server.py
+++ server.py
@@ -5,9 +5,8 @@
 
 if __name__ == '__main__':
     print('hello')
-    print('hahahha')
-    print('xxxxx')
     print('adsasdad')
     print('asdasdasd')
     print('add a asdasd asdasd asda sd')
+
 
