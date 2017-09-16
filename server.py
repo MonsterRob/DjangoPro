7adda9742ff93c0e062959a0d82a2e3cb18376bb
MonsterRob <wmwwzhao@163.com>
2017-09-16 21:37:01 +0800

    del 3 lines

--- server.py
+++ server.py
@@ -4,8 +4,6 @@
 # asdasd
 
 if __name__ == '__main__':
-    print('adsasdad')
-    print('add a asdasd asdasd asda sd')
     print('dev add a line')
     print('add a flien   asda s d asd  asd')
     print('asdasda')
